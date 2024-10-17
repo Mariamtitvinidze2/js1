54d890cbf390baf8a44b3a7f425cbfa8662e8670
mariam titvinidze <titvinidzemariam3@gmail.com>
2024-10-17 23:53:02 +0400

    js1

---
+++ script.js
@@ -0,0 +1,18 @@
+const firstname = "Mariami"
+console.log (firstname)
+
+const lastname = "Titvinidze"
+console.log (lastname)
+
+let age = 16
+console.log(age);
+
+const email = "Titvinidzemariam3@gmail.com"
+console.log (email)
+
+const number = 557775720
+console.log('%c' + number, 'color: red;')
+
+const isMarried = false;
+console.log('%c' + isMarried, 'color: yellow;');
+
